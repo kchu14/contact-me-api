0da42f5afaccce61510e35b13fd7860e66ac58d8
Kyle Chu <chu.ky@husky.neu.edu>
2020-02-28 12:07:46 -0500

    changed port listening

--- app.js
+++ app.js
@@ -50,4 +50,10 @@ app.post("/api/email", (req, res, next) => {
     });
 });
 
-app.listen(3030, "0.0.0.0");
+app.listen(process.env.PORT || 3000, function() {
+  console.log(
+    "Express server listening on port %d in %s mode",
+    this.address().port,
+    app.settings.env
+  );
+});
