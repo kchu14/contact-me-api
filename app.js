ee94968dabea8c192acd0267395aceb04eb42f8b
Kyle Chu <chu.ky@husky.neu.edu>
2020-02-28 12:20:52 -0500

    chnaged package name

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 
-const sendGrid = require("@sendGrid/mail");
+const sendGrid = require("@sendgrid/mail");
 
 const app = express();
 
